66b6dfbc46cea01e40467a73b1bf42384d60ed8f
Luonline <dood0@live.ca>
2015-03-22 22:50:50 -0300

    Added a person and item class
    Actually put information in the person and item class. These objects are
    currently not being used in the game.

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,41 @@
-
+//items the player can find along the way in the map
 public class Item {
 
+	private String name;
+	private int damage;
+	private int durability;
+	
+	
+	public Item(String n, int d, int u){
+		name = n;
+		damage = d;
+		durability = u;
+		
+	}
+	
+	//get methods
+	public String getName(){
+		return name;
+	}
+	
+	public int getDamage(){
+		return damage;
+	}
+	
+	public int getDur(){
+		return durability;
+	}
+	
+	//set methods
+	//When used the durability of an item goes down
+	//If it reaches 0 it is destroyed
+	public void setDur(int x){
+		durability-=x;
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,31 @@
  * This is the person.
  * Put inventory here I guess?
  */
+import java.util.ArrayList;
 public class Person {
 
+	private ArrayList<Item> inv = new ArrayList<Item>();
+	
+	
+	public Person(){}
+	
+	//Max inv space is currently 5
+	public boolean addItem(Item a){
+		
+		boolean ret = false;
+		
+		if(inv.size()>=5)
+			System.out.println("Not enough inventory space");
+		else{
+			inv.add(a);
+			ret = true;
+		}
+		
+		return ret;
+		
+	}
+	
+	
+	
+	
 }
