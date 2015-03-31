5371da6860ec055826779afc8559aeea57389d17
supernintendochalmers <f1431140@trbvm.com>
2015-03-30 22:11:49 -0300

    hp and attack
    I added some hp and attack attributes.

--- src/Person.java
+++ src/Person.java
@@ -4,11 +4,16 @@
  */
 import java.util.ArrayList;
 public class Person {
-
+	private int hp;
+	private int attack;
+	
 	private ArrayList<Item> inv = new ArrayList<Item>();
 	
 	
-	public Person(){}
+	public Person(){
+		hp = 100;
+		attack = 50;
+	}
 	
 	//Max inv space is currently 5
 	public boolean addItem(Item a){
@@ -25,6 +30,7 @@ public class Person {
 		return ret;
 		
 	}
+	
 	
 	public ArrayList<Item> getInventory() {
 		return inv;
